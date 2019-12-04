032cf75074f459f69c8729a80f66b9a6061f4d70
Iain J. Watson <iain+github@ij-watson.co.uk>
2019-12-04 13:02:01 +0000

    Day 4 solution

---
+++ Day4/part1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import day4, sys
+
+start = int(sys.argv[1])
+end = int(sys.argv[2])
+
+results = list()
+cur_num = start
+
+while cur_num <= end:
+    if not day4.length_is_six(cur_num):
+        cur_num += 1
+        continue
+
+    if not day4.in_range(start, end, cur_num):
+        cur_num += 1
+        continue
+
+    if not day4.increases(cur_num):
+        cur_num += 1
+        continue
+
+    if not day4.has_double(cur_num):
+        cur_num += 1
+        continue
+
+    results.append(cur_num)
+
+print("There are %s possible passwords" % len(results))
+print(results)
